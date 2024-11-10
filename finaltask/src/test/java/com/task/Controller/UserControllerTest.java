630c232a527468242be21ad2f05e03eb56497501
Harishraj20 <sharish29102002@gmail.com>
2024-11-10 20:29:39 +0530

    Test cases Added

--- finaltask/src/test/java/com/task/Controller/UserControllerTest.java
+++ finaltask/src/test/java/com/task/Controller/UserControllerTest.java
@@ -1,18 +1,27 @@
 package com.task.Controller;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.web.servlet.MockMvc;
-import org.junit.Test;
 import com.task.Model.User;
 import com.task.Service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ExtendWith(SpringExtension.class)
+import static org.mockito.Mockito.when;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "classpath:RequestServlet-servlet.xml") 
+@WebAppConfiguration
 public class UserControllerTest {
 
     @Mock
@@ -21,23 +30,65 @@ public class UserControllerTest {
     @InjectMocks
     private UserController userController;
 
-    private User user;
     private MockMvc mockMvc;
+    private User user;
 
-    @BeforeEach
+    @Before
     public void setUp() {
+        MockitoAnnotations.openMocks(this);  
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
+
         user = new User();
         user.setUserName("Harish");
         user.setPassword("Harish@1");
     }
 
-
     @Test
-    public void testHomePage(){
-
-
-
-
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"));
     }
 
+    @Test
+    public void testAddUser() throws Exception {
+        when(userService.addUsers(user)).thenReturn("User added successfully");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/addUser")
+                .param("userName", "Harish")
+                .param("password", "Harish@1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("message"))
+                .andExpect(MockMvcResultMatchers.model().attribute("msg", "User \"Harish\" Created Successfully!!"));
+    }
+
+    @Test
+    public void testViewUsers() throws Exception {
+        when(userService.fetchDetails()).thenReturn(List.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/Views"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("Details"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("userList"));
+    }
+
+    @Test
+    public void testLoginUser() throws Exception {
+        when(userService.verifyLogin(user)).thenReturn("Login successful");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                .param("userName", "Harish")
+                .param("password", "Harish@1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("message"))
+                .andExpect(MockMvcResultMatchers.model().attribute("msg", "Login successful"));
+    }
+
+    @Test
+    public void testGoBack() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/back"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
+    }
 }
--- finaltask/src/test/java/com/task/Service/UserServiceTest.java
+++ finaltask/src/test/java/com/task/Service/UserServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -41,4 +43,43 @@ public class UserServiceTest {
         verify(userRepository, times(1)).addUserInfo(user); 
     }
 
+    @Test
+    public void testFetchDetails() {
+        User user1 = new User();
+        user1.setUserName("Harish");
+        user1.setPassword("Harish@1");
+
+        User user2 = new User();
+        user2.setUserName("John");
+        user2.setPassword("John@2");
+
+        List<User> expectedUsers = List.of(user1, user2);
+
+        when(userRepository.getUsers()).thenReturn(expectedUsers);
+
+        List<User> result = userService.fetchDetails();
+
+        assertEquals(expectedUsers, result);
+        verify(userRepository, times(1)).getUsers();
+    }
+
+    @Test
+    public void testVerifyLogin_Success() {
+        when(userRepository.validateLogin(user)).thenReturn("Login successful");
+
+        String result = userService.verifyLogin(user);
+
+        assertEquals("Login successful", result);
+        verify(userRepository, times(1)).validateLogin(user);
+    }
+
+    @Test
+    public void testVerifyLogin_Failure() {
+        when(userRepository.validateLogin(user)).thenReturn("Invalid credentials");
+
+        String result = userService.verifyLogin(user);
+
+        assertEquals("Invalid credentials", result);
+        verify(userRepository, times(1)).validateLogin(user);
+    }
 }
